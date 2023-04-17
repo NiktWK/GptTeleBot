ec914496b874bb8fac6d7052d24e1b71c13369f0
NiktWK <niktwork@yandex.ru>
2023-04-17 19:18:36 +0700

    lal

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 os.environ["TELEGRAM_BOT_TOKEN"] = "6132201222:AAElYETIcG15lsuE_zX5sfWyHGE6dtjkceM"
 s = "sk-6132201222:AAElYETIcG15lsuE_zX5sfWyHGE6dtjkceM"
--- handlers.py
+++ handlers.py
@@ -5,17 +5,25 @@ from aiogram.dispatcher import FSMContext
 
 async def tell(message: types.Message):
     try:
+        print(message.text)
         await bot.send_chat_action(message.chat.id, types.ChatActions.TYPING)
         gpt = GPT(str(message.from_user.id))
         await message.answer(gpt.tell(message.text))
-    except:
-        await message.answer('Извините, попробуйте еще раз.')
+    except Exception as er:
+        await message.answer(f'Извините, попробуйте еще раз. {er}')
+
 
 async def start(message: types.Message):
     s = """
     Привет! Я - бот для общения с ChatGPT - моделью ИИ для генерации текста.
     А, точно, что это я.. Пусть она сама о себе расскажет: "Я - онлайн-бот, созданный компанией OpenAI. Я написан таким образом, чтобы помочь людям в их повседневной жизни, понимать и отвечать на их вопросы, предоставляя им полезную информацию. Я умею поддерживать беседу с людьми, общаться на различных языках и предоставлять ответы на широкий спектр вопросов в различных областях, включая науку, искусство, историю, спорт и другие. Надеюсь, что моя работа будет полезна для пользователей и поможет им получить нужную информацию быстро и легко."
     Вот так вот. Чтобы начать общение с ChatGPT напиши любую фразу.
+    """
+    await message.answer(s)
+
+async def help(message: types.Message):
+    s = """
+    
     """
     await message.answer(s)
 
